306e811b337d0aa4638e3e2ab82edb4c1e803457
unknown <estiven.carmona@udea.edu.co>
2023-04-10 11:03:52 -0500

    Cambio de deploy de la API

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize-cockroachdb");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
